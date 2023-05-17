75db641a54a9e46b0282b5873e566a6afce75108
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-17 23:38:43 +1000

    Программа проверки деления
    Введенное число делится на 3 и 7

---
+++ Independent_work_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace Independent_work_1
+{
+    class Independent_work_1
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            MessageBox.Show("Выполняется программа", "Начало");
+
+            try
+            {
+                number = Int32.Parse(Interaction.InputBox("Введите число которое делится на 3 и 7:", "Число"));
+
+                if (number % 3 == 0)
+                {
+                    if (number % 7 == 0)
+                    {
+                        MessageBox.Show("Да, число делится на 3 и 7!", "Число");
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Нет, число НЕделится на 3 и 7!", "Число");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели не число!!!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
