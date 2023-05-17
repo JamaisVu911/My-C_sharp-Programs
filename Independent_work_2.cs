aedd277d800c388af64d98d4e494038d193d3c66
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-18 00:05:25 +1000

    Сравнение двух введённых чисел

---
+++ Independent_work_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace Independent_work_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOne, numberTwo;
+
+            try
+            {
+                numberOne = Int32.Parse(Interaction.InputBox("Введите первое число", "Первое число"));
+                numberTwo = Int32.Parse(Interaction.InputBox("Введите второе число", "Второе число"));
+
+                if (numberOne > numberTwo)
+                {
+                    MessageBox.Show("Первое число больше", "Результат");
+                }
+                if(numberOne < numberTwo)
+                {
+                    MessageBox.Show("Второе число больше", "Результат");
+                }
+                if (numberOne == numberTwo)
+                {
+                    MessageBox.Show("Числа равны", "Результат");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели не числа!!!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
