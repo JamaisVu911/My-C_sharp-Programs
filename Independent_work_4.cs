545a23d87b5bf6d31769e5b0b715321fb26fa6fa
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-18 22:48:55 +1000

    Определение дня недели по числу

---
+++ Independent_work_4.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Independent_work_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string week;
+
+            try
+            {
+                number = Int32.Parse(Interaction.InputBox("Введите число от 1 до 7", "Число"));
+
+                switch(number)
+                {
+
+                    case 1:
+                        week = "Понедельник";
+                        break;
+                    case 2:
+                        week = "Вторник";
+                        break;
+                    case 3:
+                        week = "Среда";
+                        break;
+                    case 4:
+                        week = "Четверг";
+                        break;
+                    case 5:
+                        week = "Пятница";
+                        break;
+                    case 6:
+                        week = "Суббота";
+                        break;
+                    case 7:
+                        week = "Воскресенье";
+                        break;
+                    default:
+                        week = "Некорректное число";
+                        break;
+                }
+                MessageBox.Show(week, "День недели");
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели не числа!!!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
