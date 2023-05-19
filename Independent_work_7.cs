e59485c4f26bad2115cd9be78173440d1538f5a2
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-19 23:25:11 +1000

    Вычисление суммы чётных чисел

---
+++ Independent_work_7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace Independent_work_7
+{
+    class Independent_work_7
+    {
+        static void Main(string[] args)
+        {
+            int n = 100, k = 1, s = 0;
+
+            Console.WriteLine("Сумма 2 + 4 + 6 + .... + {0} = ", 2 * n);
+
+            while(k <= n)
+            {
+                s += n + 1;
+                k++;
+            }
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
