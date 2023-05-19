e662c804a0e2910b66bc3064118da0539d53c9e3
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-19 21:56:58 +1000

    Определение порядкового номера дня недели

---
+++ Independent_work_5.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Independent_work_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string week;
+            string number;
+
+            week = Interaction.InputBox("Введите день недели", "Ввод дня недели");
+
+            if (week == "Понедельник")
+            {
+                number = "Первый";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            else if (week == "Вторник")
+            {
+                number = "Второй";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            
+            else if (week == "Среда")
+            {
+                number = "Третий";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            
+            else if (week == "Четверг")
+            {
+                number = "Четвертый";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            
+            else if (week == "Пятница")
+            {
+                number = "Пятый";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            
+            else if (week == "Суббота")
+            {
+                number = "Шестой";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            
+            else if (week == "Воскресенье")
+            {
+                number = "Седьмой";
+                MessageBox.Show("Введенный день недели " + number);
+            }
+            
+            else MessageBox.Show("Такого дня нет");
+        }
+    }
+}
