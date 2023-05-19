2b0c15503c272480d7c90873b4df91de13824895
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-20 00:03:10 +1000

    Вычисление суммы чисел возводимых в квадрат
    Выполнено 3-мя разными циклами: while, do-while, for

---
+++ Independent_work_8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Independent_work_8
+{
+    class Independent_work_8
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 10, k, s;
+
+            Console.WriteLine("Сумма 1 * 1 + 2 * 2 + 3 * 3 + .... + {0} = ", n * n);
+
+            //while(k <= n) Вариант №1
+            //{
+            //s += k * k;
+            //k++;
+            //}
+
+            //do  Вариант №2
+            //{
+            //s += k * k;
+            //k++;
+            //}
+
+            //while (k <= n);
+
+            for (k = 1, s = 0; k <= n; s += k * k, k++) ; // Вариант №3
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+        }
+    }
+}
