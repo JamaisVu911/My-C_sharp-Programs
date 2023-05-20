e397bd099227b2df65da2b5d3f5795a868254ea0
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-20 11:43:10 +1000

    Написание последовательности целых чисел
    От наименьшего к наибольшему 3-мя разными циклами

---
+++ Independent_work_10.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Independent_work_10
+{
+    class Independent_work_10
+    {
+        static void Main(string[] args)
+        {
+            int numberone, numbertwo, numberMAX, numberMin;
+            
+
+            try
+            {
+                Console.WriteLine("Введите первое число: ");
+                numberone = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число: ");
+                numbertwo = Int32.Parse(Console.ReadLine());
+
+                if (numberone > numbertwo)
+                {
+                    numberMAX = numberone;
+                    numberMin = numbertwo;
+                }
+                else
+                {
+                    numberMAX = numbertwo;
+                    numberMin = numberone;
+                }
+
+                Console.Write(numberMin + " , ");
+
+                // int subsequence = numberMin + 1; // Вариант №1 и №2
+
+                /* Вариант №1
+                 
+                while (subsequence < numberMAX)
+                {
+                    Console.Write(subsequence + " , ");
+                    subsequence++;
+                }
+
+                */
+
+                /* Вариант №2
+                do
+                {
+                    Console.Write(subsequence + " , ");
+                    subsequence++;
+                }
+                while (subsequence < numberMAX);
+
+                */
+
+                for(int subsequence = numberMin + 1; subsequence < numberMAX; subsequence++)
+                {
+                    Console.Write(subsequence + " , ");
+                }
+
+                Console.Write(numberMAX);
+
+            }
+            catch
+            {
+                Console.WriteLine("Введено не число!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
